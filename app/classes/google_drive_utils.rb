daf19ee10cbc36302f67311820730e38e870dfdb
Jacob Foley <jacob.j.foley@gmail.com>
2015-08-05 15:24:56 +1000

    Removed require from GoogleFileUtils. Fixes #98

--- app/classes/google_drive_utils.rb
+++ app/classes/google_drive_utils.rb
@@ -1,10 +1,3 @@
-require 'google/api_client'
-require 'google/api_client/client_secrets'
-require 'google/api_client/auth/installed_app'
-require 'google/api_client/auth/storage'
-require 'google/api_client/auth/storages/file_store'
-require 'fileutils'
-
 class GoogleDriveUtils
 
   CLIENT_ID = ENV["GOOGLE_CLIENT_ID"]
