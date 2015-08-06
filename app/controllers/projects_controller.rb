1778f6333c7699352e15fd2974f8408208f3a5c5
Jacob Foley <jacob.j.foley@gmail.com>
2015-08-06 19:02:10 +1000

    Projects ordered by name.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -52,7 +52,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = @user.projects
+    @projects = @user.projects.order(:name)
   end
 
   # GET /projects/1
