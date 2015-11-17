36bcb29f7f91e2e6680293f1fc8b1bfb6276f1aa
Jacob Foley <jacob.j.foley@gmail.com>
2015-11-17 16:41:16 +1100

    Added measurement class for easy summaries.

---
+++ app/utilities/measurement.rb
@@ -0,0 +1,74 @@
+class Measurement
+
+  # Constructor, given the measurement's name and an array of values.
+  def initialize(name, values)
+
+    @name = name
+    @values = values
+  end
+
+  # Find how many values are stored.
+  def count
+
+    # Return the count of values.
+    return @values.count
+  end
+
+  # Find the minimum value.
+  def min
+
+    # Return min.
+    return @values.min
+  end
+
+  # Find the maximum value.
+  def max
+
+    # Return max.
+    return @values.max
+  end
+
+  # Find mean.
+  def mean
+
+    # Return the mean average.
+    return (@values.reduce(:+).to_f / count).round(2)
+  end
+
+  # Find variance.
+  def variance
+
+    # Cache the mean.
+    m = mean
+
+    # Calculate variance.
+    amount = @values.reduce(0.0) { |total, x| total + (x - m) ** 2 } / count
+
+    # Return variance.
+    return amount.round(2)
+  end
+
+  # Find standard deviation.
+  def std_dev
+
+    # Return standard deviation.
+    return Math.sqrt(variance).round(2)
+  end
+
+  # Output for displays.
+  def display_terminal
+
+    # Define average string.
+    averages = "#{mean} (#{min}-#{max}, σ: #{std_dev})"
+
+    # Neatly display values.
+    return "#{@name}: #{count} items, averaging #{averages}"
+  end
+
+  # Output for tables.
+  def display_csv
+
+    # Display in comma separated value format.
+    return "#{@name},#{count},#{mean},#{min},#{max},#{std_dev}"
+  end
+end
